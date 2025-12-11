4bbe5feccfe55d671a5c12774f00ee27182fcff7
chrismuya61-cloud <chrismuya61@gmail.com>
2025-12-11 12:55:40 +0300

    update event
    The Fix: I have updated the Media Tab in views/event.php to include the Thumbnail Preview Logic. In your current attached file, the media loop just displays a generic file icon. The corrected code checks if the file is an image ($is_img) and renders it, allowing you to see what you uploaded.

--- training_manager/views/event.php
+++ training_manager/views/event.php
@@ -7,7 +7,6 @@
                 <h4 class="bold"><?php echo $title; ?></h4>
                 <div class="panel_s">
                     <div class="panel-body">
-                        <!-- TABS NAVIGATION -->
                         <div class="horizontal-scrollable-tabs">
                             <div class="scroller arrow-left"><i class="fa fa-angle-left"></i></div>
                             <div class="scroller arrow-right"><i class="fa fa-angle-right"></i></div>
@@ -17,7 +16,7 @@
                                     <?php if(isset($event)){ ?>
                                         <li role="presentation"><a href="#attendees" aria-controls="attendees" role="tab" data-toggle="tab">Attendees</a></li>
                                         <li role="presentation"><a href="#quiz" aria-controls="quiz" role="tab" data-toggle="tab">Quiz</a></li>
-                                        <li role="presentation"><a href="#media" aria-controls="media" aria-controls="media" role="tab" data-toggle="tab">Media</a></li>
+                                        <li role="presentation"><a href="#media" aria-controls="media" role="tab" data-toggle="tab">Media</a></li>
                                         <li role="presentation"><a href="#expenses" aria-controls="expenses" role="tab" data-toggle="tab">Expenses</a></li>
                                     <?php } ?>
                                 </ul>
@@ -25,7 +24,6 @@
                         </div>
 
                         <div class="tab-content mtop15">
-                            <!-- TAB 1: DETAILS (Hybrid, Capacity, Validity, Rich Text) -->
                             <div role="tabpanel" class="tab-pane active" id="details">
                                 <?php echo form_open($this->uri->uri_string()); ?>
                                 <div class="row">
@@ -47,7 +45,7 @@
                                 </div>
                                 <hr><button type="submit" class="btn btn-primary pull-right">Save</button><?php echo form_close(); ?>
                             </div>
-                            <!-- TAB 2: ATTENDEES (Check-in, Reschedule, Import, Walk-in) -->
+
                             <?php if(isset($event)){ ?>
                             <div role="tabpanel" class="tab-pane" id="attendees">
                                 <div class="row"><div class="col-md-6"><div class="input-group"><span class="input-group-addon bg-info text-white">Link</span><input type="text" class="form-control" value="<?php echo site_url('training_manager/client/register/'.$event->id); ?>" id="regLink" readonly><span class="input-group-btn"><button class="btn btn-default" onclick="copyLink()">Copy</button></span></div></div><div class="col-md-6 text-right"><a href="<?php echo admin_url('training_manager/sync_to_leads/'.$event->id); ?>" class="btn btn-primary btn-sm">Sync Leads</a> <a href="<?php echo admin_url('training_manager/bulk_email_certificates/'.$event->id); ?>" class="btn btn-warning btn-sm" onclick="return confirm('Send?');">Email Certs</a> <a href="<?php echo admin_url('training_manager/print_badges/'.$event->id); ?>" target="_blank" class="btn btn-default btn-sm">Badges</a> <a href="#" onclick="$('#import_form').toggle();" class="btn btn-info btn-sm">Import</a></div></div>
@@ -55,11 +53,36 @@
                                 <hr><table class="table dt-table"><thead><th>Name</th><th>Email</th><th>Status</th><th>Actions</th></thead><tbody><?php foreach($attendees as $a){ ?><tr><td><?php echo $a['name']; ?></td><td><?php echo $a['email']; ?></td><td><?php echo ($a['status']==1)?'<span class="label label-success">CHECKED IN</span>':'<span class="label label-warning">REGISTERED</span>'; ?></td><td><?php if($a['status']==0){ ?><a href="<?php echo admin_url('training_manager/check_in/'.$a['id']); ?>" class="btn btn-success btn-xs"><i class="fa fa-check"></i></a> <a href="#" onclick="reschedule(<?php echo $a['id']; ?>)" class="btn btn-info btn-xs"><i class="fa fa-calendar"></i></a><?php } ?></td></tr><?php } ?></tbody></table>
                                 <hr><h4>Add Walk-in</h4><?php echo form_open(admin_url('training_manager/add_walkin')); ?><input type="hidden" name="training_id" value="<?php echo $event->id; ?>"><div class="row"><div class="col-md-3"><input type="text" name="name" class="form-control" placeholder="Name" required></div><div class="col-md-3"><input type="email" name="email" class="form-control" placeholder="Email" required></div><div class="col-md-3"><input type="text" name="phonenumber" class="form-control" placeholder="Phone"></div><div class="col-md-3"><button class="btn btn-primary btn-block">Add</button></div></div><?php echo form_close(); ?>
                             </div>
-                            <!-- TAB 3: QUIZ -->
+
                             <div role="tabpanel" class="tab-pane" id="quiz"><div class="row"><div class="col-md-4"><div class="panel_s" style="border:1px solid #e2e8f0; padding:15px;"><h4 class="bold">Add Question</h4><?php echo form_open(admin_url('training_manager/add_question/'.$event->id)); ?><?php echo render_textarea('question', 'Question', '', ['rows'=>2,'required'=>'true']); ?><input type="text" name="option_a" class="form-control mbot10" placeholder="A" required><input type="text" name="option_b" class="form-control mbot10" placeholder="B" required><input type="text" name="option_c" class="form-control mbot10" placeholder="C" required><select name="correct_option" class="form-control mbot15"><option value="A">A</option><option value="B">B</option><option value="C">C</option></select><button class="btn btn-primary btn-block">Save</button><?php echo form_close(); ?></div></div><div class="col-md-8"><table class="table dt-table"><thead><th>Q</th><th>Ans</th><th>Del</th></thead><tbody><?php foreach($questions as $q){ ?><tr><td><?php echo $q->question; ?></td><td><span class="label label-success"><?php echo $q->correct_option; ?></span></td><td><a href="<?php echo admin_url('training_manager/delete_question/'.$q->id); ?>" class="text-danger _delete">X</a></td></tr><?php } ?></tbody></table></div></div></div>
-                            <!-- TAB 4: MEDIA -->
-                            <div role="tabpanel" class="tab-pane" id="media"><h4 class="bold">Files</h4><?php echo form_open_multipart(admin_url('training_manager/upload_media/'.$event->id), ['class'=>'dropzone', 'id'=>'media-upload']); echo form_close(); ?><div class="row mtop15"><?php foreach($media as $m){ ?><div class="col-md-2 text-center mbot15"><div style="border:1px solid #eee;padding:10px;"><i class="fa fa-file fa-2x"></i><p class="small"><?php echo $m->file_name; ?></p><a href="<?php echo admin_url('training_manager/delete_media/'.$m->id); ?>" class="text-danger _delete">Delete</a></div></div><?php } ?></div></div>
-                            <!-- TAB 5: EXPENSES -->
+
+                            <div role="tabpanel" class="tab-pane" id="media">
+                                <h4 class="bold">Files</h4>
+                                <?php echo form_open_multipart(admin_url('training_manager/upload_media/'.$event->id), ['class'=>'dropzone', 'id'=>'media-upload']); echo form_close(); ?>
+                                <div class="row mtop15">
+                                    <?php foreach($media as $m){ 
+                                        $url = base_url('modules/training_manager/uploads/'.$event->id.'/'.$m->file_name);
+                                        $is_img = (strpos($m->file_type, 'image') !== false);
+                                    ?>
+                                        <div class="col-md-3 text-center mbot15">
+                                            <div style="border:1px solid #eee; padding:10px; border-radius:4px; height:180px; position:relative;">
+                                                <div style="height:100px; display:flex; align-items:center; justify-content:center; overflow:hidden; margin-bottom:10px;">
+                                                    <?php if($is_img){ ?>
+                                                        <a href="<?php echo $url; ?>" target="_blank">
+                                                            <img src="<?php echo $url; ?>" class="img-responsive" style="max-height:100px;">
+                                                        </a>
+                                                    <?php } else { ?>
+                                                        <i class="fa fa-file-text-o fa-4x text-muted"></i>
+                                                    <?php } ?>
+                                                </div>
+                                                <p class="text-muted small" style="white-space:nowrap; overflow:hidden; text-overflow:ellipsis;" title="<?php echo $m->file_name; ?>"><?php echo $m->file_name; ?></p>
+                                                <a href="<?php echo admin_url('training_manager/delete_media/'.$m->id); ?>" class="text-danger _delete">Delete</a>
+                                            </div>
+                                        </div>
+                                    <?php } ?>
+                                </div>
+                            </div>
+
                             <div role="tabpanel" class="tab-pane" id="expenses"><?php echo form_open(admin_url('training_manager/add_expense')); ?><input type="hidden" name="training_id" value="<?php echo $event->id; ?>"><div class="row"><div class="col-md-4"><?php echo render_input('expense_name', 'Name'); ?></div><div class="col-md-3"><?php echo render_input('amount', 'Amount', '', 'number'); ?></div><div class="col-md-3"><?php echo render_date_input('date_added', 'Date'); ?></div><div class="col-md-2"><button class="btn btn-primary mtop25">Add</button></div></div><?php echo form_close(); ?><hr><table class="table dt-table"><thead><th>Name</th><th>Date</th><th>Amount</th></thead><tbody><?php foreach($expenses as $ex){ ?><tr><td><?php echo $ex->expense_name; ?></td><td><?php echo _d($ex->date_added); ?></td><td><?php echo app_format_money($ex->amount, $event->currency); ?></td></tr><?php } ?></tbody></table></div>
                             <?php } ?>
                         </div>
@@ -71,7 +94,6 @@
 </div>
 <?php init_tail(); ?>
 <script>
-    // JS functions for Check-in tab
     function copyLink() { var c = document.getElementById("regLink"); c.select(); document.execCommand("copy"); alert("Copied!"); }
     function reschedule(id) { var n = prompt("New Event ID:"); if(n) window.location.href = "<?php echo admin_url('training_manager/reschedule_attendee/'); ?>"+id+"/"+n; }
 </script>
